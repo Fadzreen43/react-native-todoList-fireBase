c59145ba098c094e59c06c4244fc6aa18a90a857
fadzreen43 <fadzreen34@gmail.com>
2020-03-23 04:48:14 +0800

    React Native & Firebase Todo App Part 1

---
+++ tempData.js
@@ -0,0 +1,57 @@
+// temporary Data until we connect with Firebase
+
+
+export default tempData = [
+    {
+        name: " Plan a Trip",
+        color: "#24A6D9",
+        todos: [
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title: "Passport Check",
+                completed: true
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed: true
+            },
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title: "Pack Luggage",
+                completed: false
+            }
+        ]
+    },
+    {
+        name: " Birthday Party",
+        color: "#222D39",
+        todos: [
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title: "Passport Check",
+                completed: false
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed: true
+            },
+            {
+                title: "Book Flight",
+                completed: true
+            },
+            {
+                title: "Pack Luggage",
+                completed: true
+            }
+        ]
+    }
+];
